c07ce481a0aacdbeb8e002d30615d7b3b5b108b8
baeksnip <76125829+baeksnip@users.noreply.github.com>
2022-09-07 08:27:46 +0200

    Update ipChange.py

--- ipChange.py
+++ ipChange.py
@@ -3,7 +3,7 @@
 #This is a simple utility that I needed in my day to day, which informs me by mail if my public ip changes
 #By automating the script it alerts you by email when the public IP changes
 #For example with crontab I would check every 5 minutes
-#*/5 * * * * /ROUTE_TO/ipChange.py
+#*/5 * * * * /ROUTE_TO_SCRIPT/ipChange.py
 #The script consists of two python files and a text file to store the previous ip:
 #ipChange.py (PRINCIPAL SCRIPT)
 #config.py (CONFIGURATION FILE FOR EMAIL)
