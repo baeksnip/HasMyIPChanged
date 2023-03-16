7c148953c2f6b4586cee47d6d1dd6698e4d9b65d
baeksnip <76125829+baeksnip@users.noreply.github.com>
2023-03-16 10:17:14 +0100

    Update and rename ipChange.py to HasMyIPChanged.py

--- HasMyIPChanged.py
+++ HasMyIPChanged.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+# HasMyIPChanged?
 #This is a simple utility that I needed in my day to day, which informs me by mail if my public ip changes
 #By automating the script it alerts you by email when the public IP changes
 #For example with crontab I would check every 5 minutes
